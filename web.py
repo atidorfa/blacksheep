2528ba7d9230cf7f887c4a0c57242ca738d4da85
Gonzalo Rivero Baud <riverobaudg@gmail.com>
2022-05-28 22:17:32 -0300

    (finger counter) open web with 5

---
+++ web.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+
+def open_google():
+    webbrowser.open("http://google.com")
+
+
+def open_youtube():
+    webbrowser.open(
+        "https://www.youtube.com/watch?v=mqfzsbV0lUs&ab_channel=GYMBODYMOTIVATION"
+    )
