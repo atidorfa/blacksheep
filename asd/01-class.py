2ea21f3cd0be4ef484ab7a5fe292d7ac94170f1f
Gonzalo Rivero Baud <riverobaudg@gmail.com>
2022-12-10 18:15:25 -0300

    asd/01-class.py added

--- asd/01-class.py
+++ asd/01-class.py
@@ -2,3 +2,39 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+    def __del__(self):
+        print("Object is being deconstructed!")
+
+
+p = Person("Harvey", 27)
+
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    # Overriding addition function
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __repr__(self):
+        # How to represent the Vector when we need it
+        return f"X: {self.x}, Y: {self.y}"
+
+    # def __str__(self):
+    #     # What happens when is type cast the Vector into sting.
+    #     return "This is a vector!"
+
+
+v1 = Vector(10, 20)
+v2 = Vector(50, 60)
+
+# We need the definition of the addition
+# TypeError: unsupported operand type(s) for +: 'Vector' and 'Vector'
+v3 = v1 + v2
+
+print(v3)
+print(v3.x)
+print(v3.y)
