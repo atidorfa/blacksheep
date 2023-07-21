29ac110ecc4950d50892f07946f99e0d4aad3f84
atidorfa <riverobaudg@gmail.com>
2023-07-21 20:06:23 -0300

    logging_example added

---
+++ asd/logging_example.py
@@ -0,0 +1,20 @@
+import logging
+
+
+def main():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s]: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        # filename="logs.log",
+    )
+
+    logging.debug("This is a debug message")
+    logging.info("This is an info message")
+    logging.warning("This is a warning message")
+    logging.error("This is an info message")
+    logging.critical("This is an info message")
+
+
+if __name__ == "__main__":
+    main()
